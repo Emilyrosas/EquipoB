5a8f3aee6285316cd5c2c0040dd236663d072d9b
alinelara091 <162637449+alinelara091@users.noreply.github.com>
2025-04-01 12:48:59 -0600

    Delete mi_infierno directory

--- mi_infierno/scriptalex.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var chatContainer = document.querySelector(".chat-container");
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-});
